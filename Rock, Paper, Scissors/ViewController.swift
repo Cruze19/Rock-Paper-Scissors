64d061be6464ed883240e48e66759e2176bde2a5
Cruze19 <enisc787@gmail.com>
2025-04-22 15:30:55 -0400

    Update viewController

--- Rock, Paper, Scissors/ViewController.swift
+++ Rock, Paper, Scissors/ViewController.swift
@@ -23,16 +23,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI(forState: .start)
     }
     
     // MARK: IB Action
     @IBAction func rockChosen(_ sender: Any) {
+        play(userSign: .rock)
     }
     @IBAction func paperChosen(_ sender: Any) {
+        play(userSign: .paper)
     }
     @IBAction func scissorsChosen(_ sender: Any) {
+        play(userSign: .scissors)
     }
     @IBAction func playAgain(_ sender: Any) {
+        updateUI(forState: .start)
     }
     
     // MARK: - Instance Methods
@@ -41,15 +46,13 @@ class ViewController: UIViewController {
         
         switch state {
         case .start:
-            view.backgroundColor = .lightGray
+            view.backgroundColor = .cyan
             
             signLabel.text = "👽"
             playAgainButton.isHidden = true
-            
             rockButton.isHidden = false
             paperButton.isHidden = false
             scissorsButton.isHidden = false
-            
             rockButton.isEnabled = true
             paperButton.isEnabled = true
             scissorsButton.isEnabled = true
@@ -69,6 +72,25 @@ class ViewController: UIViewController {
         let gameState = userSign.gameState(against: computerSign)
         updateUI(forState: gameState)
         
+        signLabel.text = computerSign.emoji
+        
+        rockButton.isHidden = true
+        paperButton.isHidden = true
+        scissorsButton.isHidden = true
+        rockButton.isEnabled = false
+        paperButton.isEnabled = false
+        scissorsButton.isEnabled = false
+        
+        switch userSign {
+        case .rock:
+            rockButton.isHidden = false
+        case .paper:
+            paperButton.isHidden = false
+        case .scissors:
+            scissorsButton.isHidden = false
+        }
+        
+        playAgainButton.isHidden = false
     }
 
 }
